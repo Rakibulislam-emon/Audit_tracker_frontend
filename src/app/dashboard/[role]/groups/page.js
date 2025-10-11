6d5b069993ce2fd69db7a4d524fc2463cb0ff1b9
Rakibulislam-emon <rakibulislamemon60@gmail.com>
2025-10-12 00:07:53 +0600

    updating for checking

--- src/app/dashboard/[role]/groups/page.js
+++ src/app/dashboard/[role]/groups/page.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function GroupManagementPage() {
   return (
-    <div>GroupManagementPage Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt alias doloremque nulla deleniti soluta! Tempora, numquam delectus nam tempore magnam ab repellendus quia pariatur voluptate quasi et, incidunt veniam at.</div>
+    <div>GroupManagementPage  here will be group management things</div>
   )
 }
